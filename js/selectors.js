dd2bfbc1204953d93088f25f77df26eb03ef4511
andy19910102 <andy19910102@gmail.com>
2022-08-08 15:33:16 +0800

    Update css style

--- js/selectors.js
+++ js/selectors.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.container');
-const a = document.querySelector('.container>article');
+const a = document.querySelector('.container>div');
 
 container.innerHTML = `<label class="label">.container</label>` + container.innerHTML;
-document.querySelector('.container>article').innerHTML = `<label class="label">.container article</label>` + document.querySelector('.container>article').innerHTML;
+document.querySelector('.container>div').innerHTML = `<label class="label">.container div</label>` + document.querySelector('.container>div').innerHTML;
